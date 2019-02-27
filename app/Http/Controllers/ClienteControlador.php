5bc3e04ff0d441302b66a4ca2bda9bcedda7607d
SamuelHenrique32 <shdalmas@ucs.br>
2019-02-27 15:39:29 -0300

    Aula 189 - Paginacao com JavaScript Criando rotas

--- app/Http/Controllers/ClienteControlador.php
+++ app/Http/Controllers/ClienteControlador.php
@@ -7,82 +7,24 @@ use App\Cliente;            //modelo
 
 class ClienteControlador extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    /*
     public function index()
     {
         //$clientes = Cliente::all();
         //10 itens por vez
         $clientes = Cliente::paginate(10);
         return view('index', compact('clientes'));
+    }*/
+
+    public function indexjs()
+    {
+
+        return view('indexjs');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function indexjson()
     {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return Cliente::paginate(10);
     }
 }
---
+++ resources/views/indexjs.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{csrf_token()}}">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        <title>Paginação</title>
+        <style>
+            body{
+                padding: 20px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="card text-center">
+                <div class="card-header">
+                    Tabela de Clientes
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <table class="table table-hover">
+                        <thead>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Sobrenome</th>
+                            <th scope="col">E-mail</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Marcel</td>
+                                <td>Teodoro</td>
+                                <td>marcel@gmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+
+                </div>
+            </div>
+        </div>
+        <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+    </body>
+
+</html>
